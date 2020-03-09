879a53d66c6c076dc081786b6f3079fc11fce8f1
Alice Freda <alicefreda@gmail.com>
2020-03-08 19:40:39 -0700

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,35 @@
+class SchoolClassesController < ApplicationController
+
+    before_action :set_school_class, only: [:show, :edit, :update]
+
+
+    def new
+        @school_class = SchoolClass.new
+    end
+    
+    
+	def create
+		@school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+		redirect_to school_class_path(@school_class)
+	end
+    
+    def show
+
+    end
+    
+    def edit
+
+    end
+
+    def update
+		@school_class.update(params.require(:school_class).permit(:title, :room_number))
+		redirect_to school_class_path(@school_class)
+    end
+
+    private
+
+    def set_school_class
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,35 @@
+class StudentsController < ApplicationController
+
+    before_action :set_student, only: [:show, :edit, :update]
+
+
+    def new
+        @student = Student.new
+    end
+    
+    
+	def create
+		@student = Student.create(params.require(:student).permit(:first_name, :last_name))
+		redirect_to student_path(@student)
+	end
+    
+    def show
+
+    end
+    
+    def edit
+
+    end
+
+    def update
+		@student.update(params.require(:student).permit(:first_name, :last_name))
+		redirect_to student_path(@student)
+    end
+
+    private
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
+
+end
